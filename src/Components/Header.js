fa00da15f19a4b3c120d28027c7c5463906a3ee2
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-01 17:47:35 -0500

    made adjustments for mobile use and changed the header font

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,29 +5,25 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <div className="overlay"></div>
-        <video
-          playsInLine="playsinline"
-          autoPlay="autoplay"
-          muted="muted"
-          loop="loop"
-        >
-          <source src={video} type="video/mp4" />
-        </video>
-        <div className="container-o ">
-          <div className="">
-            <div className="w-100 text-white ">
-              <h1 className="main-title w-100 display-3">ADOPT ME</h1>
-              <p className="lead mb-0">
-                Connecting you with your next companion
-              </p>
-            </div>
+    <header>
+      <div className="overlay"></div>
+      <video
+        playsInLine="playsinline"
+        autoPlay="autoplay"
+        muted="muted"
+        loop="loop"
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="container-o ">
+        <div className="">
+          <div className="w-100 text-white ">
+            <h1 className="main-title w-100 display-3">ADOPT ME</h1>
+            <p className="lead mb-0">Connecting you with your next companion</p>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };
 
