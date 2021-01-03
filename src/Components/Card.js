959c4397db20fb9db29d568adb03a92b853be9fe
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-02 21:48:23 -0500

    trunicated each animal description to a specific character limit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,14 @@
 import "./Card.css";
 
 export function Card({ animal }) {
-  const fixJSON = (str) => str.replace(/&#39;/g, "'").replace(/&nbsp;/g, " ");
+  const fixJSON = (str) =>
+    str
+      .replace(/&#39;/g, "'")
+      .replace(/&nbsp;/g, " ")
+      .replace(/&quot;/g, '"')
+      .replace(/&rsquo;/g, "'")
+      .replace(/&lsquo;/g, "'");
+  const trunicateStr = (str) => str.slice(0, 500) + "...";
   return (
     <div className="card" key={animal.id}>
       <img
@@ -10,10 +17,10 @@ export function Card({ animal }) {
       />
       <h4>{animal.attributes.breedPrimary}</h4>
       <h5>{animal.attributes.sex}</h5>
-      <h6>{fixJSON(`${animal.attributes.descriptionText}`)}</h6>
       <h5>
-        {animal.attributes.ageString ? animal.attributes.ageString : "N/A"}
+        Age: {animal.attributes.ageString ? animal.attributes.ageString : "N/A"}
       </h5>
+      <h6>{trunicateStr(fixJSON(`${animal.attributes.descriptionText}`))}</h6>
     </div>
   );
 }
