b09b69f9ec9e66a5c5f36ee1204ffe0de5bd6fa3
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-02 21:23:12 -0500

    added a function to fix the JSON that contained HTML character entities and displayed that in the cards. The cards will be updated to have a limited text length

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
 import "./Card.css";
 
 export function Card({ animal }) {
+  const fixJSON = (str) => str.replace(/&#39;/g, "'").replace(/&nbsp;/g, " ");
   return (
     <div className="card" key={animal.id}>
       <img
@@ -8,6 +9,11 @@ export function Card({ animal }) {
         alt={animal.attributes.breedPrimary}
       />
       <h4>{animal.attributes.breedPrimary}</h4>
+      <h5>{animal.attributes.sex}</h5>
+      <h6>{fixJSON(`${animal.attributes.descriptionText}`)}</h6>
+      <h5>
+        {animal.attributes.ageString ? animal.attributes.ageString : "N/A"}
+      </h5>
     </div>
   );
 }
